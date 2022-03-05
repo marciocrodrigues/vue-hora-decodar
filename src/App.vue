dc54b555c993fe158ed4fd0126d4858887fd53f5
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2022-03-05 12:39:11 -0300

    Hierarqui de componentes

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <PrimeiroComponente />
     <LifeCycle />
+    <Pessoa />
   </div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
 import LifeCycle from './components/LifeCycle.vue';
+import Pessoa from './components/Pessoa.vue';
 
 export default {
   components: { 
     PrimeiroComponente,
-    LifeCycle 
+    LifeCycle,
+    Pessoa
   },
   name: 'App'
 }
---
+++ src/components/Info.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as seguintes tecnologias</p>
+        <ul>
+            <li>Javascriopt</li>
+            <li>C#</li>
+            <li>ReactJs</li>
+            <li>Angular</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Info'
+}
+</script>
