2da81991054f8aa9c1f80cb94ba5de18a00cb46c
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2022-03-07 17:04:18 -0300

    diretivas: v-if, v-else, v-show atributos dinamicos: v-bind, : -> v-bind sintaxe resumida

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <p>Estou trabalhando no momento.</p>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else>Estou em busca de novas oportunidades</p>
         <p>Utilizo as seguintes tecnologias</p>
         <ul>
             <li>Javascriopt</li>
@@ -8,11 +9,25 @@
             <li>ReactJs</li>
             <li>Angular</li>
         </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">basta clicar aqui</a></p>
+        <Picture />
     </div>
 </template>
 
 <script>
+import Picture from './Picture.vue';
+
 export default {
-    name: 'Info'
+    components: { Picture },
+    name: 'Info',
+    data() {
+        return {
+            esta_trabalhando: true,
+            mostrar_email: true,
+            email: 'teste@teste.com.br',
+            meu_link: 'https://www.google.com.br'
+        }
+    }
 }
 </script>
