73c6558cbf32701f3d4f861c685e7988d83ae763
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2022-03-12 19:37:20 -0300

    eventos @submit e @click

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <h2>Meu formulario</h2>
-        <form action="">
+        <form action="" @submit="enviarFormulario($event)">
             <div>
-                <InputText />
+                <input type="text" v-model="name" />
             </div>
             <div>
-                <InputText />
+                <input type="text" v-model="email" />
             </div>
             <Submit />
         </form>     
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
+        <p>Conteúdo</p>
     </div>
 </template>
 
@@ -23,6 +26,27 @@ export default {
     components: {
         InputText,
         Submit
+    },
+    data() {
+        return {
+            name: '',
+            email: ''
+        }
+    },
+    methods: {
+        enviarFormulario(e) {
+            e.preventDefault();
+            const name = this.name;
+            const email = this.email;
+            console.log(`O nome é ${name}`);
+            console.log(`O e-mail é ${email}`);
+        }
     }
 }
 </script>
+
+<style scoped>
+    p {
+        margin: 150px;
+    }
+</style>
