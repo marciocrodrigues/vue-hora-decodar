eda3efa8c0b95728177bc9c37f3770903b367730
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2022-03-14 23:17:08 -0300

    emit

--- src/components/Picture.vue
+++ src/components/Picture.vue
@@ -1,14 +1,30 @@
 <template>
-    <img :src="avatar" :alt="descricao">
+    <div>
+        <img :src="avatar" :alt="descricao">
+        <MudarImagem @mudar-imagem="trocarImagem" />
+    </div>
 </template>
 
 <script>
+import MudarImagem from './MudarImagem.vue';
 export default {
     name: 'Picture',
+    components: {
+        MudarImagem
+    },
     data() {
         return {
             avatar: "/imagens/avatar.png",
             descricao: "Marcio"
+        }
+    },
+    methods: {
+        trocarImagem() {
+            if (this.avatar.includes('avatar2')) {
+                this.avatar = "/imagens/avatar.png";
+            } else {
+                this.avatar = "/imagens/avatar2.png";
+            }
         }
     }
 }
